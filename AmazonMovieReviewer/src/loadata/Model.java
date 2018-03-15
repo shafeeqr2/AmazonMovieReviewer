f7ecbd5de85cc484f25b2a7f49c5796f8945f28e
Shafeeq Rabbani <shafeeqr2@gmail.com>
2018-03-14 21:57:39 -0400

    FileReader (Work In Progress)

--- AmazonMovieReviewer/src/loadata/Model.java
+++ AmazonMovieReviewer/src/loadata/Model.java
@@ -1,6 +1,7 @@
 package loadata;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,31 +12,33 @@ public class Model {
 	 * Open and read a file, and return the lines in the file as a list of Strings.
 	 * (Demonstrates Java FileReader, BufferedReader, and Java5.)
 	 */
-//
-//	private static List<String> readFile(String filename) {
-//
-//		List<String> records = new ArrayList<String>();
-//		try {
-//			BufferedReader reader = new BufferedReader(new FileReader(filename));
-//			String line;
-//			while ((line = reader.readLine()) != null) {
-//				records.add(line);
-//				System.out.println(line);
-//			}
-//			reader.close();
-//			return records;
-//		} catch (Exception e) {
-//			System.err.format("Exception occurred trying to read '%s'.", filename);
-//			e.printStackTrace();
-//			return null;
-//		}
-//	}
+
+	private static List<String> readFile(String filename) {
+
+		List<String> records = new ArrayList<String>();
+		try {
+			BufferedReader reader = new BufferedReader(new FileReader(filename));
+			String line;
+			while ((line = reader.readLine()) != null) {
+				records.add(line);
+			}
+			reader.close();
+			return records;
+		} catch (Exception e) {
+			System.err.format("Exception occurred trying to read '%s'.", filename);
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
 	
 
 	public static void main(String[] args) {
 
-		System.out.println("Hi");
-//		List<String> myFile = Model.readFile("text_movies.txt");
+
+		
+		List<String> myFile = Model.readFile("./test_movies.txt");
+		
 
 	}
 
